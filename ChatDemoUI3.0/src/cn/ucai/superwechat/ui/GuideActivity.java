0782782b46b605df5977bd9d889940f53fd15a2a
limingxing1314159 <1473243403@qq.com>
2016-11-01 15:14:39 +0800

    修改包名及闪屏页面

---
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
@@ -0,0 +1,14 @@
+package cn.ucai.superwechat.ui;
+
+import android.os.Bundle;
+
+import cn.ucai.superwechat.R;
+
+public class GuideActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_guide);
+        super.onCreate(savedInstanceState);
+    }
+}
