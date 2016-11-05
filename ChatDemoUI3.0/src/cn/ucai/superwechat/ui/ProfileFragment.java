489f0811603e43a1fd5f18262943b36778b5f4a1
limingxing1314159 <1473243403@qq.com>
2016-11-05 13:42:35 +0800

    添加个人中心布局和fragment

---
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/ProfileFragment.java
@@ -0,0 +1,76 @@
+package cn.ucai.superwechat.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.easemob.redpacketui.utils.RedPacketUtil;
+import com.hyphenate.easeui.utils.EaseUserUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.ucai.superwechat.Constant;
+import cn.ucai.superwechat.R;
+
+/**
+ * Created by Administrator on 2016/11/5.
+ */
+public class ProfileFragment extends Fragment {
+    @BindView(R.id.iv_profile_avatar)
+    ImageView ivProfileAvatar;
+    @BindView(R.id.tv_profile_nickname)
+    TextView tvProfileNickname;
+    @BindView(R.id.tv_profile_username)
+    TextView tvProfileUsername;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (savedInstanceState != null && savedInstanceState.getBoolean("isConflict", false))
+            return;
+        setUserInfo();
+    }
+
+    private void setUserInfo() {
+        EaseUserUtils.setCurrentAppUserAvatar(getActivity(),ivProfileAvatar);
+        EaseUserUtils.setCurrentAppUserNick(tvProfileNickname);
+        EaseUserUtils.setCurrentAppUserNameWithNo(tvProfileUsername);
+
+    }
+
+    @OnClick({R.id.layout_profile_view, R.id.tv_profile_money, R.id.tv_profile_settings})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.layout_profile_view:
+                break;
+            //red packet code : 进入零钱页面
+            case R.id.tv_profile_money:
+                RedPacketUtil.startChangeActivity(getActivity());
+                break;
+            //end of red packet code
+            case R.id.tv_profile_settings:
+                break;
+        }
+    }
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if(((MainActivity)getActivity()).isConflict){
+            outState.putBoolean("isConflict", true);
+        }else if(((MainActivity)getActivity()).getCurrentAccountRemoved()){
+            outState.putBoolean(Constant.ACCOUNT_REMOVED, true);
+        }
+    }
+}
--- easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
+++ easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
@@ -1,11 +1,13 @@
 package com.hyphenate.easeui.utils;
 
 import android.content.Context;
+import android.support.v4.app.FragmentActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.hyphenate.chat.EMClient;
 import com.hyphenate.easeui.R;
 import com.hyphenate.easeui.controller.EaseUI;
 import com.hyphenate.easeui.controller.EaseUI.EaseUserProfileProvider;
@@ -50,10 +52,10 @@ public class EaseUserUtils {
                 Glide.with(context).load(avatarResId).into(imageView);
             } catch (Exception e) {
                 //use default avatar
-                Glide.with(context).load(user.getAvatar()).diskCacheStrategy(DiskCacheStrategy.ALL).placeholder(R.drawable.ease_default_avatar).into(imageView);
+                Glide.with(context).load(user.getAvatar()).diskCacheStrategy(DiskCacheStrategy.ALL).placeholder(R.drawable.default_hd_avatar).into(imageView);
             }
         }else{
-            Glide.with(context).load(R.drawable.ease_default_avatar).into(imageView);
+            Glide.with(context).load(R.drawable.default_hd_avatar).into(imageView);
         }
     }
     
@@ -102,5 +104,23 @@ public class EaseUserUtils {
             }
         }
     }
-    
+
+    public static void setCurrentAppUserAvatar(FragmentActivity activity, ImageView imageView) {
+        String username = EMClient.getInstance().getCurrentUser();
+        setAppUserAvatar(activity,username,imageView);
+    }
+
+    public static void setCurrentAppUserNick(TextView textView) {
+        String username = EMClient.getInstance().getCurrentUser();
+        setAppUserNick(username,textView);
+    }
+
+    public static void setCurrentAppUserNameWithNo(TextView textView) {
+        String username = EMClient.getInstance().getCurrentUser();
+        setAppUserName("微信号:",username,textView);
+    }
+
+    private static void setAppUserName(String suffix,String username, TextView textView) {
+        textView.setText(suffix+username);
+    }
 }
